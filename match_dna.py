dcb4b0bb81d44c2bd230b111aa478e587f98a802
Peter van Heusden <pvh@sanbi.ac.za>
2013-02-15 11:46:40 +0200

    Added match_dna.py

---
+++ match_dna.py
@@ -0,0 +1,14 @@
+def match_dna(query, sequence):
+    # if query is in sequence:
+    # return True
+    # else
+    # return False
+    
+    # for example
+    # match_dna('gaa', 'gaatta') returns True
+    # match_dna('aaa', 'gaatta') returns False
+
+
+mydna = 'gaaacctta'
+myquery = 'aac'
+print match_dna(myquery, mydna)
